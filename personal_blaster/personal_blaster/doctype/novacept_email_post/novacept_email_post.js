2ccee19064e16f996cd2189415ae3302a7e0d6bc
HarshMagiya7 <harshmagiya07@gmail.com>
2022-12-26 11:53:42 +0000

    bug fixes

--- personal_blaster/personal_blaster/doctype/novacept_email_post/novacept_email_post.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2022, Novacept and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Novacept Email Post', {
-	// refresh: function(frm) {
-
-	// }
-});
