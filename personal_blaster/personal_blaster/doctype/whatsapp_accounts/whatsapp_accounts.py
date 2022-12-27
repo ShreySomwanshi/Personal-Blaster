84de035d1223b07a12036135ed0f7d1f70c44a67
HarshMagiya7 <harshmagiya07@gmail.com>
2022-12-27 16:12:20 +0000

    Whatsapp Account change from child to normal doctype

---
+++ personal_blaster/personal_blaster/doctype/whatsapp_accounts/whatsapp_accounts.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, Novacept and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class WhatsappAccounts(Document):
+	pass
