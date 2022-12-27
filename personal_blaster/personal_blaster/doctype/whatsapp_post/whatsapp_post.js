0a2060b5cb3515779f38ab25a90d80be708a01f5
Novacept Toolkit <novatoolkit@novatoolkituat.dbqhy5bdzmbelcwyluylqrqzqe.fx.internal.cloudapp.net>
2022-12-27 21:02:45 +0000

    Testing

--- personal_blaster/personal_blaster/doctype/whatsapp_post/whatsapp_post.js
+++ personal_blaster/personal_blaster/doctype/whatsapp_post/whatsapp_post.js
@@ -7,10 +7,10 @@ frappe.ui.form.on('Whatsapp Post', {
 	// }
 	refresh: function(frm) {
 		if (frm.doc.post_status !== 'Posted')
-		frm.add_custom_button(__('Post Now'), function() {
+		frm.add_custom_button(__('Posts all  Now'), function() {
                         frappe.call({
-                                doc: frm.doc,
-                                method: 'message_post',
+                               // doc: frm.doc,
+                                method: 'personal_blaster.personal_blaster.doctype.whatsapp_post.whatsapp_post.process_scheduled_whatsapp_message',
                                 freeze: true,
                                 callback: function() {
                                         frm.reload_doc();
